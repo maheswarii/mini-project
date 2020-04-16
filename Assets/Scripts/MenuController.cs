3905beeb23bef57a3de67cb9c930b7025e7dcb1b
maheswarii <62419626+maheswarii@users.noreply.github.com>
2020-04-16 19:27:48 +0700

    mini project

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject audioOnIcon;
+    public GameObject audioOffIcon;
+    public Text txtHighScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       SetSoundState();
+       txtHighScore.text = PlayerPrefs.GetFloat("HighScore",0).ToString("0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void ToggleSound()
+    {
+        if (PlayerPrefs.GetInt("Muted",0) == 0)
+            {
+                PlayerPrefs.SetInt("Muted", 1);
+            }
+        else
+            {
+                PlayerPrefs.SetInt("Muted", 0);
+            }
+
+            SetSoundState();
+    }
+
+    public void SetSoundState()
+    {
+        if (PlayerPrefs.GetInt("Muted",0) == 0)
+            {
+                AudioListener.volume = 1;
+                audioOnIcon.SetActive(true);
+                audioOffIcon.SetActive(false);
+            }
+        else 
+            {
+                AudioListener.volume = 0;
+                audioOnIcon.SetActive(false);
+                audioOffIcon.SetActive(true);
+            }
+    }
+}
---
+++ Assets/Scripts/RedoController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RedoController : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+    private Animator myAnim;
+    public float redoJumpForce = 500f;
+    private float redoHurtTime = -1;
+    private Collider2D myCollider;
+    public Text scoreText;
+    private float startTime;
+    private int jumpsLeft = 2;
+    public AudioSource jumpSfx;
+    public AudioSource deathSfx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redoHurtTime == -1) {
+
+            if (Input.GetButtonUp("Jump") && jumpsLeft > 0)
+            {
+                if (myRigidBody.velocity.y < 0){
+                    myRigidBody.velocity = Vector2.zero;
+                }
+
+                if(jumpsLeft == 1){
+                    myRigidBody.AddForce(transform.up * redoJumpForce * 0.75f);
+                } else {
+                    myRigidBody.AddForce(transform.up * redoJumpForce);
+                }
+                
+                jumpsLeft--;
+
+                jumpSfx.Play();
+            } 
+
+            myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
+            scoreText.text = (Time.time - startTime).ToString("0");
+
+        } else {
+            if (Time.time > redoHurtTime + 2)
+            {
+                Application.LoadLevel("GameOver");
+                //Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
+            {
+                spawner.enabled = false;
+            }
+
+            foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>())
+            {
+                moveLefter.enabled = false;
+            }
+
+            redoHurtTime = Time.time;
+            myAnim.SetBool("redoHurt", true);
+            myRigidBody.velocity = Vector2.zero;
+            myRigidBody.AddForce(transform.up * redoJumpForce);
+            myCollider.enabled = false;
+
+            deathSfx.Play();
+
+            float currentHighScore = PlayerPrefs.GetFloat("HighScore", 0);
+            float currentScore = Time.time - startTime;
+
+            if (currentScore > currentHighScore){
+                PlayerPrefs.SetFloat("HighScore", currentScore);
+            }
+        }
+        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground")){
+            jumpsLeft = 2;
+        }
+    }
+}
